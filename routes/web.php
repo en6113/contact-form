63c0fdec4a432d013e0afa75fedca28af31ea992
en6113 <mitayan3@gmail.com>
2026-05-21 14:02:42 +0900

    認証機能の実装(Fortify)

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,41 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
+Route::get('/', function () {
+    return redirect()->route('login');
+});
+
+// 仮ルート
+Route::get('/contacts', fn() => 'お問い合わせフォーム入力ページ(準備中)')->name('contact.index');
+Route::post('/contacts/confirm', fn() => 'お問い合わせフォーム確認ページ(準備中)')->name('contact.confirm');
+Route::get('/thanks', fn() => 'サンクスページ(準備中)')->name('contacts.thanks');
+/* ContactControllerを実装したら書き直す
+Route::get('/contacts', [ContactController::class,'index'])->name('contact.index');
+Route::post('/contacts/confirm', [ContactController::class,'store'])->name('contact.confirm');
+Route::get('/thanks', [ContactController::class,'thanks'])->name('contact.thanks');
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// ログイン後仮ルート
+Route::middleware('auth')->group(function () {
+    // 管理画面
+    Route::get('/admin', fn() => '管理画面一覧（準備中）')->name('admin.index');
+    Route::get('/admin/contacts/{contact}', fn() => 'お問い合わせ詳細ページ（準備中）')->name('admin.show');
+    Route::delete('/admin/contacts/{contact}', fn() => 'お問い合わせ削除（準備中）');
+    /* AdminController実装したら書き直す
+    Route::get('/admin', [AdminController::class,'index'])->name('admin.index');
+    Route::get('/admin/contacts/{contact}', [AdminController::class,'show'])->name('admin.show');
+    Route::delete('/admin/contacts/{contact}', [AdminController::class,'destroy']);
+    */
+
+    //タグ関係
+    Route::post('/admin/tags', fn() => 'タグ追加（準備中）');
+    Route::get('/admin/tags/{tag}/edit', fn() => 'タグ編集ページ（準備中）');
+    Route::post('/admin/tags/{tag}/edit', fn() => 'タグ更新（準備中）');
+    Route::delete('/admin/tags/{tag}', fn() => 'タグ削除（準備中）');
+    /* TagController実装したら書き直す
+    Route::post('/admin/tags', [TagController::class,'store']);
+    Route::get('/admin/tags/{tag}/edit', [TagController::class,'edit']);
+    Route::post('/admin/tags/{tag}/edit', [TagController::class,'update']);
+    Route::delete('/admin/tags/{tag}', [TagController::class,'destroy'])
+    */
 });
