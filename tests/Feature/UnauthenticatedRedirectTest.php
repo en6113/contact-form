140203ca8a5a0ee2bd3e83ccad8435c7199f62e9
en6113 <mitayan3@gmail.com>
2026-05-25 16:11:10 +0900

    認証機能のテスト実装

---
+++ tests/Feature/UnauthenticatedRedirectTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\Contact;
+
+class UnauthenticatedRedirectTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function 未認証ユーザーはお問い合わせ一覧にアクセスするとログインページにリダイレクトされる(): void
+    {
+        // Act
+        $response = $this->get(route('admin.index'));
+
+        // Assert
+        $response->assertRedirect(route('login'));
+    }
+
+    /** @test */
+    public function 未認証ユーザーがお問い合わせをエクスポートしようとするとログインページにリダイレクトされる(): void
+    {
+        // Act
+        $response = $this->get(route('contact.export'));
+
+        // Assert
+        $response->assertRedirect(route('login'));
+    }
+
+    /** @test */
+    public function 未認証ユーザーはタグを保存しようとするとログインページにリダイレクトされる()
+    {
+        // Act
+        $response = $this->post(route('tag.store'), [
+            'name' => '新規タグ'
+        ]);
+
+        // Assert
+        $response->assertRedirect(route('login'));
+    }
+
+    /** @test */
+    public function 未認証ユーザーはお問い合わせを削除しようとするとログインページにリダイレクトされる()
+    {
+        // Arrange
+        $contact = Contact::factory()->create();
+
+        //Act
+        $response = $this->delete(route('admin.destroy',$contact));
+
+        // Assert
+        $response->assertRedirect(route('login'));
+        $this->assertDatabaseHas('contacts', ['id' => $contact->id]);
+    }
+}
