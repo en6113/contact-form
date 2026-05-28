d72c83dfff74ca7b232cc0d26a302cb303fd6102
en6113 <mitayan3@gmail.com>
2026-05-29 08:39:58 +0900

    feat: CategoryFactoryの修正

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -18,7 +18,13 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            'content' => fake()->word(),
+            'content' => fake()->randomElement([
+                '商品のお届けについて',
+                '商品の交換について',
+                '商品トラブル',
+                'ショップへのお問い合わせ',
+                'その他',
+            ]),
         ];
     }
 }
